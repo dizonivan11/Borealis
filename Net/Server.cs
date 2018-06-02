d43ac874efd0b4ba6f613c65e2945727c587c932
dizonivan11 <dizonivan11@gmail.com>
2018-06-02 17:17:56 +0800

    Server class added for handling clients

---
+++ Net/Server.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+public delegate void AcceptHandler(TcpClient newClient);
+
+namespace Borealis.Net {
+    public class Server {
+        TcpListener listener;
+
+        public event AcceptHandler ClientAccepted;
+        protected virtual void OnClientAccepted(TcpClient newClient) { ClientAccepted?.Invoke(newClient); }
+
+        public Server(IPEndPoint iPEndPoint) {
+            listener = new TcpListener(iPEndPoint);
+        }
+
+        public void Start() {
+            listener.Start();
+            AcceptNextClient();
+        }
+
+        private async void AcceptNextClient() {
+            TcpClient newClient = await listener.AcceptTcpClientAsync();
+            OnClientAccepted(newClient);
+            AcceptNextClient();
+        }
+    }
+}
