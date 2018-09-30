b69aba9c7f952fbdcecda116986aa62c470b599e
dizonivan11 <dizonivan11@gmail.com>
2018-10-01 02:10:19 +0800

    Added ThreadSafeExtensions class for form controls.

---
+++ Net/ThreadSafeExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+public delegate void ThreadSafeHandler();
+
+namespace Borealis.Net {
+
+    public static class ThreadSafeExtensions {
+        public static void RunSafely(this Control control, ThreadSafeHandler action) {
+            if (control.InvokeRequired) {
+                control.Invoke(action);
+            } else {
+                action.Invoke();
+            }
+        }
+    }
+}
