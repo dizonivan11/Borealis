6543771f32fd8694025d3140d6e96598f4345072
dizonivan11 <dizonivan11@gmail.com>
2018-12-01 21:11:14 +0800

    Added helper method AddNew

--- Net/Networks.cs
+++ Net/Networks.cs
@@ -2,6 +2,10 @@
 
 namespace Borealis.Net {
     public class Networks : Dictionary<Network, Dictionary<string, dynamic>> {
-        // TODO: Add helper methods
+
+        // TODO: Add many helper methods
+        public void AddNew(Network newNetwork) {
+            Add(newNetwork, new Dictionary<string, dynamic>());
+        }
     }
 }
